345b632c4e0470382b47a5c5e75b513cceec2456
Thiago Cesar Andrade <90931093+AndradeTC86@users.noreply.github.com>
2024-08-07 10:01:05 -0300

    Update BasePage.js

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -1,49 +1,16 @@
-const { Builder, By } = require('selenium-webdriver')
+const SeleniumFactory = require('./seleniumFactory')
 
 class BasePage{
     static classPageTitle = 'title'
 
     constructor(driver = null, browser = null){
-        if (driver){
-            this.driver = driver
-        }
-        else{
-            switch (browser){
-                case 'chrome':
-                    this.driver = new Builder().forBrowser('chrome').build()
-                    break
-                case 'firefox':
-                    this.driver = new Builder().forBrowser('firefox').build()
-                    break
-                case 'edge':
-                    this.driver = new Builder().forBrowser('MicrosoftEdge').build()
-                    break
-                default:
-                    throw new Error('Browser não suportado!')
-            }
-            this.driver.manage().window().maximize()
-            this.driver.manage.setTimeouts({ implicit: 3000})
-        }
+        this.driver = driver
+        this.seleniumFactory = new SeleniumFactory(this.driver)
     }
 
-    async close(){
-        await this.driver.quit()
+    async initElements(){
+        
     }
-
-    async isUrlPage(url){
-        const currentUrl = await this.driver.getCurrentUrl()
-        return currentUrl === url
-    }
-
-    async hasPageTitle(titleText){
-        const pageTitle = await this.driver.findElement(By.className(PageObject.caller)).getText()
-        return pageTitle === titleText
-    }
-
-    async checkPage(url, titleText){
-        return await this.isUrlPage(url) && await this.hasPageTitle(titleText)
-    }
-
 }
 
 module.exports = BasePage
